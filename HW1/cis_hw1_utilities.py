f3004fe4205ca2edd1f7cae14bc366314f1a0de9
Yupeng Li <yupengli@seas.upenn.edu>
2020-02-04 17:02:29 -0500

    added py file for submission

--- HW1/cis_hw1_utilities.py
+++ HW1/cis_hw1_utilities.py
@@ -15,37 +15,7 @@
 import random 
 import numpy as np
 import pandas as pd
-from sklearn import tree
-# from sklearn.model_selection import train_test_split
-from sklearn.impute import SimpleImputer
 random.seed(42)  # don't change this line
-
-
-# In[ ]:
-
-
-# Load all data tables
-# baseDir = '/Users/yupengli/CIS519/HW1/'
-df = pd.read_csv('hw1-NHANES-diabetes-train.csv')
-
-# Output debugging info
-print(df.shape)
-df.head()
-
-
-# In[ ]:
-
-
-# Print information about the dataset
-print('Percentage of instances with missing features:')
-print(df.isnull().sum(axis=0)/df.shape[0])
-print()
-print('Class information:')
-print(df['DIABETIC'].value_counts())
-
-
-# In[ ]:
-
 def addDummyFeatures(inputDf, feature):
     """
     Create a one-hot-encoded version of a categorical feature and append it to the existing
@@ -113,17 +83,7 @@ def addDummyFeatures(inputDf, feature):
 # 
 # 
 # 
-X = df.drop(['SEQN','DIABETIC','DIQ010'],axis=1)
-y = df.loc[:,'DIABETIC']
-for feature in X.columns:
-    if X[feature].isnull().sum(axis=0)/X.shape[0] >= 0.5:
-        X = X.drop(feature,axis=1)
-    elif X[feature].dtypes == 'O':
-        X = addDummyFeatures(X,feature)
-imp = SimpleImputer(missing_values=np.nan, strategy='mean')
-imp.fit(X)
-X = pd.DataFrame(imp.transform(X),columns = X.columns)
-    
+
     
 # X = X.fillna(0)
 
@@ -131,88 +91,7 @@ X = pd.DataFrame(imp.transform(X),columns = X.columns)
 # In[ ]:
 
 
-# TODO Insert your preprocessing code here
-def convertToBinary(inputDf, feature):
-    rows,columns = inputDf.shape #
-    for i in range(rows):
-        if pd.isna(inputDf.loc[i,feature]):
-            pass
-        else :
-            value_0 = inputDf.loc[i,feature]#Record the first value in the column
-            break
-    second_Value_Raised = False
-    for i in range(rows):
-        if inputDf.loc[i,feature] == value_0:
-            inputDf.loc[i,feature] = 0
-        elif inputDf.loc[i,feature] != value_0 and (not second_Value_Raised):
-            value_1 = inputDf.loc[i,feature]#Record second value in the column
-            second_Value_Raised = True
-            inputDf.loc[i,feature] = 1
-        elif inputDf.loc[i,feature] == value_1:
-            inputDf.loc[i,feature] = 1
-        elif pd.isna(inputDf.loc[i,feature]):
-            pass
-        else:
-            print (f'{feature} is not a binary feature')# If there is a different value other than the first two, raise error
-            return(None)
 
-    outDf = inputDf
-    return outDf
-
-def addDummyFeatures(inputDf, feature):
-    """
-    Create a one-hot-encoded version of a categorical feature and append it to the existing
-    dataframe.
-
-    After one-hot encoding the categorical feature, ensure that the original categorical feature is dropped
-    from the dataframe so that only the one-hot-encoded features are retained.
-
-    For more on one-hot encoding (OHE) : https://hackernoon.com/what-is-one-hot-encoding-why-and-when-do-yo u-have-to-use-it-e3c6186d008f
-
-    Arguments:
-        inputDf (Pandas.DataFrame): input dataframe
-        feature (str) : Feature for which the OHE is to be performed
-
-
-    Returns:
-        outDf (Pandas.DataFrame): Resultant dataframe with the OHE features appended and the original feature removed
-
-    """
-
-
-    ## TODO ##
-    if feature not in inputDf.columns:
-        return('Feature not in dataset')
-    rows,columns = inputDf.shape
-    feature_List = []
-    OHE_Matrix = np.array([[]]) #Create a matrix to store the OHE values
-    for i in range(rows):
-        if pd.isna(inputDf.loc[i,feature]):
-            OHE_Matrix = np.concatenate((OHE_Matrix,np.zeros((1,len(feature_List)))),axis=0) #If missing data, create a new row of zeros
-        elif str(inputDf.loc[i,feature]) not in feature_List:
-            feature_List.append(str(inputDf.loc[i,feature]))
-            OHE_Matrix = np.concatenate((OHE_Matrix,np.zeros((i+1,1))),axis=1)#if there is a new feature, create a new column of zeros
-        if str(inputDf.loc[i,feature]) in feature_List:
-            OHE_Matrix = np.concatenate((OHE_Matrix,np.zeros((1,len(feature_List)))),axis=0)#if this it is alreay in feature list , create a new row of zeros  and set the feature related column to 1
-            OHE_Matrix[i,feature_List.index(str(inputDf.loc[i,feature]))]=1
-    for i in range(len(feature_List)):
-        feature_List[i] = feature + '_'+feature_List[i]#New column names for OHE
-
-    OHE_Matrix = np.delete(OHE_Matrix,rows,0)#Delete the extra row created
-
-    dataOut= pd.DataFrame(OHE_Matrix,columns=feature_List) #Create a dataframe with OHE as matrix and the new feature list
-    outDf = pd.concat([inputDf,dataOut],axis=1)#Concate new features to original matrix
-    outDf = outDf.drop(feature,axis=1)#drop the original feature
-    return outDf
-
-# ## **Modeling**
-# 
-# In this section, you are tasked with building a Decision Tree classifier to predict whether or not a patient has diabetes. The overall goal of this exercise is to investigate the dataset and develop features that would improve your model performance.
-# 
-# To help with this process, we have provided the structure for two helper functions. These functions will help in tuning your model as well as validating your model's performance.
-# 
-# Complete these two functions.
-# 
 # 
 
 # In[ ]:
@@ -332,23 +211,3 @@ def automatic_dt_pruning(DecisionTreeClassifier, X, y, num_trials, num_folds, ra
 # Once you are done with your modeling process, test your model on the test dataset and output your predictions in a file titled "cis519_hw1_predictions.csv", with one row per prediction.
 
 # In[ ]:
-
-dtc = tree.DecisionTreeClassifier(criterion = 'entropy')
-result_ccp_alpha = automatic_dt_pruning(dtc, X, y, 10, 10, 42)
-dtc.set_params(ccp_alpha = result_ccp_alpha)
-
-dtc.fit(X,y)
-
-import graphviz 
-dot_data = tree.export_graphviz(dtc, out_file=None) 
-graph = graphviz.Source(dot_data) 
-graph.render("Unpruned Tree") 
-dot_data = tree.export_graphviz(dtc, out_file=None, 
-                     feature_names=X.columns,  
-                     class_names=['None', 'Diabetes'],  
-                     filled=True, rounded=True,  
-                     special_characters=True)  
-graph = graphviz.Source(dot_data) 
-graph     
-## TODO ##
-
